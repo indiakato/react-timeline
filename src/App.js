d55270bbf551dce77b70c4369dd76288eb6ae672
India Kato <india.kato@gmail.com>
2020-12-27 14:02:39 -0800

    changed app title

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">{timelineData.person}</h1>
+        <h1 className="App-title">{timelineData.person}'s Social Media Feed</h1>
       </header>
       <main className="App-main">
         < Timeline events={timelineData.events} />
