d81e11f5ef579a2b8810864cbb0e978ed76579c2
India Kato <india.kato@gmail.com>
2020-12-27 12:58:00 -0800

    timeline event

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,8 +2,14 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  return;
+const TimelineEvent = (props) => {
+  return (
+    <ul>
+      <li>{props.person}</li>
+      <li>{props.status}</li>
+      <li>< Timestamp time={props.Timestamp} /></li>
+    </ul>
+  )
 }
 
 export default TimelineEvent;
